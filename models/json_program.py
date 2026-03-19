c4d0ea209f9e89bf7883670cbfa5cf982dfa357e
delaconcepcion.carlota <delaconcepcion.carlota@gmail.com>
2026-03-19 12:16:59 +0100

    updates jsons

---
+++ models/json_program.py
@@ -0,0 +1,11 @@
+import json
+def read_json(json_path, position):
+    print(position)
+    with open(json_path, 'r') as file:
+        data_dict = json.load(file) 
+        
+    for key in data_dict:
+        print(key, data_dict[key][position])
+    return 
+
+read_json(r'D:\Master\UAM_MUBBC\2o_cuatri\BioinfEstructural\project\STRBIO_P2\models\Hog1\fold_osm1_monomer_171p\fold_osm1_monomer_171p_full_data_0.json',173)
